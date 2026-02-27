fd6658325e45bf20ce1efbcd83a7696421ea0843
camille benteux <benteuxc@gmail.com>
2026-02-27 12:14:31 +0100

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "lists#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lists, only: [ :index, :show, :new, :create, :destroy ] do
     resources :bookmarks, only: [ :new, :create ]
